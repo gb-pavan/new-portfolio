6225319e4bb289b7f868a566a7e8f963eb79e59a
pavan <pavankmr1902@gmail.com>
2024-10-31 12:01:32 +0530

    added some more changes

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -7,7 +7,7 @@ const Projects = () => {
     {      image: 'https://res.cloudinary.com/db9tsiti3/image/upload/v1730353903/swagger_zjnlgy.png', technology:"NextJS-Express-Swagger", url: 'https://ornate-fox-4aeb46.netlify.app/api',      description: 'Contact Management - Backend',    },
     {      image: 'https://res.cloudinary.com/db9tsiti3/image/upload/v1730353903/ecommerceapp_flmecp.png', technology:"React JS", url: 'https://ornate-fox-4aeb46.netlify.app/',      description: 'E-commerce Application',    },  
     {      image: 'https://res.cloudinary.com/db9tsiti3/image/upload/v1682926303/Screenshot_2023-05-01_130027_kr8zpr.png', technology:"Full Stack Application", url: 'https://gb-pavan.github.io/Banking-Client/',      description: 'Banking Application',    },
-    {      image: 'https://res.cloudinary.com/db9tsiti3/image/upload/v1681361805/festivals_jgvxrr.png',technology:"React JS",  url: 'https://gb-pavan.github.io/festival-Events-Status-ReactJS/',      description: 'Festival Event Status',    },    
+    // {      image: 'https://res.cloudinary.com/db9tsiti3/image/upload/v1681361805/festivals_jgvxrr.png',technology:"React JS",  url: 'https://gb-pavan.github.io/festival-Events-Status-ReactJS/',      description: 'Festival Event Status',    },    
     {      image: 'https://res.cloudinary.com/db9tsiti3/image/upload/v1681648560/quiz_hlwwas.png',technology:"React JS", url: 'https://gb-pavan.github.io/Quiz-using-React/',      description: 'Quiz',    },    
         
     {      image: 'https://res.cloudinary.com/db9tsiti3/image/upload/v1681649069/typing_ep1zsc.png',technology:"Javascript", url: 'https://testtyping207.ccbp.tech/',      description: 'Speed Typing Test',    },    
